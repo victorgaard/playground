18a57a38d274c5e813d36f81e5f82228a0fac3cb
Victor F. Santos <victor.gaard@gmail.com>
2024-01-12 15:44:27 +0100

    chore: refactor props generator

--- src/components/Button.playground.ts
+++ src/components/Button.playground.ts
@@ -1,45 +1,29 @@
 import { generateProps } from "../utils/generateProps";
 import { ButtonProps } from "./Button";
 
-/** Set default props for the component */
-const defaultProps: ButtonProps = {
-  children: "👉 click me",
-  variant: "primary",
-  size: "md",
-  loading: false,
-};
-
-/** Set playground examples */
-const secondary: ButtonProps = {
-  children: "🥈 secondary button",
-  variant: "secondary",
-  size: "md",
-  loading: false,
-};
-const destructive: ButtonProps = {
-  children: "👀 uh-oh danger zone",
-  variant: "destructive",
-  size: "md",
-  loading: false,
-};
-
-/** Set multiple variants, if there are any */
-const variant: ButtonProps["variant"][] = [
-  "primary",
-  "secondary",
-  "destructive",
-];
-const size: ButtonProps["size"][] = ["sm", "md", "lg"];
-
-/** Finally, generate props */
 export const props = generateProps<ButtonProps>({
-  defaultProps,
+  defaultProps: {
+    children: "👉 click me",
+    variant: "primary",
+    size: "md",
+    loading: false,
+  },
   multipleProps: {
-    variant,
-    size,
+    variant: ["primary", "secondary", "destructive"],
+    size: ["sm", "md", "lg"],
   },
   examples: {
-    secondary,
-    destructive,
+    secondary: {
+      children: "🥈 secondary button",
+      variant: "secondary",
+      size: "md",
+      loading: false,
+    },
+    destructive: {
+      children: "👀 uh-oh danger zone",
+      variant: "destructive",
+      size: "md",
+      loading: false,
+    },
   },
 });
--- src/components/Input.playground.ts
+++ src/components/Input.playground.ts
@@ -1,48 +1,37 @@
 import { generateProps } from "../utils/generateProps";
 import { InputProps } from "./Input";
 
-/** Set default props for the component */
-const defaultProps: InputProps = {
-  label: "Input label",
-  placeholder: "I'm a placeholder...",
-  defaultValue: "",
-  error: "",
-  variant: "rest",
-  autoFocus: true,
-  disabled: false,
-};
-
-/** Set playground examples */
-const success: InputProps = {
-  label: "Best doggo",
-  placeholder: "I'm a placeholder...",
-  defaultValue: "All doggos are the best ones",
-  error: "",
-  variant: "success",
-  autoFocus: true,
-  disabled: false,
-};
-const error: InputProps = {
-  label: "Dog food",
-  placeholder: "I'm a placeholder...",
-  defaultValue: "ded birbo",
-  error: "Yikes, are you sure?",
-  variant: "error",
-  autoFocus: true,
-  disabled: false,
-};
-
-/** Set multiple variants, if there are any */
-const variant: InputProps["variant"][] = ["rest", "success", "error"];
-
-/** Finally, generate props */
 export const props = generateProps<InputProps>({
-  defaultProps,
+  defaultProps: {
+    label: "Input label",
+    placeholder: "I'm a placeholder...",
+    defaultValue: "",
+    error: "",
+    variant: "rest",
+    autoFocus: true,
+    disabled: false,
+  },
   multipleProps: {
-    variant,
+    variant: ["rest", "success", "error"],
   },
   examples: {
-    success,
-    error,
+    success: {
+      label: "Best doggo",
+      placeholder: "I'm a placeholder...",
+      defaultValue: "All doggos are the best ones",
+      error: "",
+      variant: "success",
+      autoFocus: true,
+      disabled: false,
+    },
+    error: {
+      label: "Dog food",
+      placeholder: "I'm a placeholder...",
+      defaultValue: "ded birbo",
+      error: "Yikes, are you sure?",
+      variant: "error",
+      autoFocus: true,
+      disabled: false,
+    },
   },
 });
