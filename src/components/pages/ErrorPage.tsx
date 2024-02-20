c5184d381b847da376af82b0ecf071c4cd09cc5f
Victor F. Santos <victor.gaard@gmail.com>
2024-02-20 20:34:28 +0100

    chore: use alert on error message

--- src/components/pages/ErrorPage.tsx
+++ src/components/pages/ErrorPage.tsx
@@ -1,16 +1,19 @@
 import { errorRoute } from "@/main";
 import { ExclamationCircleIcon } from "@heroicons/react/24/outline";
+import Alert from "../ui/Alert/Alert";
 
 function ErrorPage() {
   const { error } = errorRoute.useSearch<{ error: string }>();
 
   return (
     <div className="flex h-full flex-1 flex-col gap-4 p-8">
-      <h1>There was an error</h1>
       {error && (
-        <div className="flex items-center gap-2 rounded-lg border border-red-900 bg-red-950 p-3 text-red-200">
-          <ExclamationCircleIcon className="h-5 w-5" /> Error: {error}
-        </div>
+        <Alert
+          variant="error"
+          title="Error"
+          message={error}
+          icon={<ExclamationCircleIcon className="h-5 w-5" />}
+        />
       )}
     </div>
   );
